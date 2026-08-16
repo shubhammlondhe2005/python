46bc2f2f76ad94bfbdb411b761de415b74878ce2
Shubham londhe <shubhammlondhe@gmail.com>
2026-08-16 17:58:26 +0530

    Add files via upload

---
+++ numpy/2 array property/agg_fun.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr=np.array([1,2,3,4,5])
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.min(arr))
+print(np.max(arr))
+print(np.std(arr))
+print(np.var(arr))
---
+++ numpy/2 array property/astype.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+arr=np.array([1.2,5.2,4.6])
+newarr=arr.astype(int)
+print(newarr)
+print(newarr.dtype)
