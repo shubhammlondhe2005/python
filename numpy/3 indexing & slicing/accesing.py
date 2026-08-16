d31df2c7e7df7af75f9bd6e9ccd6ec9a32e60778
Shubham londhe <shubhammlondhe@gmail.com>
2026-08-16 17:59:53 +0530

    Add files via upload

---
+++ numpy/3 indexing & slicing/accesing.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr=np.array([10,20,30,40,50])
+print(arr[0])
+
+arr2=np.array([
+              [1,2,3],
+              [4,5,6],
+              [7,8,9]])
+print(arr2[2,2])
---
+++ numpy/3 indexing & slicing/boolean masking.py
@@ -0,0 +1,4 @@
+import numpy as np
+arr=np.array([10,20,30,40,50,60,70])
+
+print(arr[arr>30])
