e7f3e5dcfc382ea9efbebdeff72f8957185c7f47
Shubham londhe <shubhammlondhe@gmail.com>
2026-08-16 18:09:37 +0530

    Add files via upload

---
+++ numpy/8 handling missing value/infinity.py
@@ -0,0 +1,3 @@
+import numpy as np
+arr=np.array([1,2,np.inf,4,-np.inf,6])
+print(np.isinf(arr))
---
+++ numpy/8 handling missing value/replace.py
@@ -0,0 +1,6 @@
+import numpy as np
+arr=np.array([1,2,np.inf,4,-np.inf,6])
+print(np.isinf(arr))
+
+cleaned_arr=np.nan_to_num(arr,posinf=10000,neginf=-1000)
+print(cleaned_arr)
