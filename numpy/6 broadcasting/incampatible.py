a164834f75ac5042e569ebf981211cbfb829ec70
Shubham londhe <shubhammlondhe@gmail.com>
2026-08-16 18:06:30 +0530

    Add files via upload

---
+++ numpy/6 broadcasting/incampatible.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+matrix=np.array([[1,2,3],[4,5,6]]) #(2,3)matrix
+vector=np.array([10,20])               #2 column 
+
+result=matrix+vector
+print(result)
---
+++ numpy/6 broadcasting/single value.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+value=np.array([100,200,300])
+result=value*2
+print(result)
